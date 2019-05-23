6c0d007232d96f2c1b5bbb79c7196311b5a12831
Max Woolf <max@minimaxir.com>
2019-05-23 12:14:28 -0700

    Reorder statements for consistency

--- ray_tensorflow.py
+++ ray_tensorflow.py
@@ -20,19 +20,17 @@ ray.init(num_cpus=num_threads,
 @ray.remote(num_cpus=num_threads / (concurrency+1))
 class ParameterServer(object):
     def __init__(self, cluster_addresses, num_threads):
+        self.var = tf.Variable(0.0, name='var')
 
         cluster = tf.train.ClusterSpec(cluster_addresses)
-
-        with tf.device("/job:ps/task:0"):
-            self.var = tf.Variable(0.0, name='var')
+        server = tf.train.Server(cluster,
+                                 job_name="ps",
+                                 task_index=0)
 
         config = tf.ConfigProto()
         config.intra_op_parallelism_threads = num_threads
         config.inter_op_parallelism_threads = num_threads
 
-        server = tf.train.Server(cluster,
-                                 job_name="ps",
-                                 task_index=0)
         self.sess = tf.Session(target=server.target, config=config)
 
         print("Parameter server: initializing variables...")
@@ -43,8 +41,7 @@ class ParameterServer(object):
 @ray.remote(num_cpus=num_threads / (concurrency+1))
 class Worker(object):
     def __init__(self, cluster_addresses, worker_n):
-        with tf.device("/job:ps/task:0"):
-            self.var = tf.Variable(0.0, name='var')
+        self.var = tf.Variable(0.0, name='var')
 
         cluster = tf.train.ClusterSpec(cluster_addresses)
         server = tf.train.Server(cluster,
