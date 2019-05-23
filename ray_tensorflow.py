751dbd7921f854ea6f0846959aad31eb95efe843
Max Woolf <max@minimaxir.com>
2019-05-23 11:48:07 -0700

    Parameterize num_threads

--- ray_tensorflow.py
+++ ray_tensorflow.py
@@ -4,6 +4,7 @@ from time import sleep
 import asyncio
 from ray.experimental import async_api
 
+num_threads = 1
 concurrency = 2
 
 cluster_addresses = {
@@ -11,14 +12,14 @@ cluster_addresses = {
     'ps': ["localhost:3000"]
 }
 
-ray.init(num_cpus=1,
+ray.init(num_cpus=num_threads,
          object_store_memory=100 * 1000000,
          redis_max_memory=100 * 1000000)
 
 
-@ray.remote(num_cpus=1 / (concurrency+1))
+@ray.remote(num_cpus=num_threads / (concurrency+1))
 class ParameterServer(object):
-    def __init__(self, cluster_addresses):
+    def __init__(self, cluster_addresses, num_threads):
 
         cluster = tf.train.ClusterSpec(cluster_addresses)
 
@@ -26,8 +27,8 @@ class ParameterServer(object):
             self.var = tf.Variable(0.0, name='var')
 
         config = tf.ConfigProto()
-        config.intra_op_parallelism_threads = 1
-        config.inter_op_parallelism_threads = 1
+        config.intra_op_parallelism_threads = num_threads
+        config.inter_op_parallelism_threads = num_threads
 
         server = tf.train.Server(cluster,
                                  job_name="ps",
@@ -39,7 +40,7 @@ class ParameterServer(object):
         print("Parameter server: variables initialized")
 
 
-@ray.remote(num_cpus=1 / (concurrency+1))
+@ray.remote(num_cpus=num_threads / (concurrency+1))
 class Worker(object):
     def __init__(self, cluster_addresses, worker_n):
         with tf.device("/job:ps/task:0"):
@@ -66,7 +67,7 @@ class Worker(object):
         print(self.sess.run(self.var))
 
 
-ps = ParameterServer.remote(cluster_addresses)
+ps = ParameterServer.remote(cluster_addresses, num_threads)
 worker_list = [Worker.remote(cluster_addresses, i) for i in range(concurrency)]
 
 loop = asyncio.get_event_loop()
